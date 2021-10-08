234c0df98e152cc422d94bf07d7f5e94582832ef
Allogo Loic <brownshans@gmail.com>
2021-10-08 15:33:49 +0100

    completer le formulaire de contact

--- to-email.php
+++ to-email.php
@@ -4,7 +4,8 @@ if (!isset($_POST['submit'])) {
 }
 $name = $_POST['nom'];
 $client_mail = $_POST['client_mail'];
-// $message = $_POST['message'];
+$message = $_POST['message'];
+$contact = $_POST['contact'];
 
 #Première  validation
 if (empty($name) || empty('$client_mail')) {
@@ -13,10 +14,11 @@ if (empty($name) || empty('$client_mail')) {
 }
 
 $email_from = 'digitalakeng@gmail.com'; #Il s'agit de votre mail 
-$email_subject = 'Nouvel envoie soumise!';
+$email_subject = 'Votre mail a bien été envoyer!';
 $email_body = "Vous avez réçu un message de la part de l'utilisateur $name. \n".
 "adresse email: $client_mail\n".
-// "Ici est le message de: \n $message".
+"Ici est le message de: \n $message".
+"Ici est son numéro: \n $contact".
 
 $to = "digitalakeng@gmail.com";
 $header = "From: $email_from \r \n";
@@ -25,35 +27,6 @@ $header = "From: $email_from \r \n";
 
 mail($to,$email_subject,$email_body,$header);
 
-// function IsInjected($str)
-// {
-//     $injections = array('(\n+)',
-//            '(\r+)',
-//            '(\t+)',
-//            '(%0A+)',
-//            '(%0D+)',
-//            '(%08+)',
-//            '(%09+)'
-//            );
-               
-//     $inject = join('|', $injections);
-//     $inject = "/$inject/i";
-    
-//     if(preg_match($inject,$str))
-//     {
-//       return true;
-//     }
-//     else
-//     {
-//       return false;
-//     }
-// }
-
-// if(IsInjected($client_email))
-// {
-//     echo "Bad email value!";
-//     exit;
-// }
 
 header("location:index.html");
 ?>
